33af68e0e963b918b96b01600ef9acfdc4eddf6d
Ryan Thompson <service@aiwebsystems.com>
2015-01-08 06:39:17 -0600

    Noice

--- src/Ui/Form/ExampleFormBuilder.php
+++ src/Ui/Form/ExampleFormBuilder.php
@@ -7,5 +7,17 @@ class ExampleFormBuilder extends FormBuilder
 
     protected $model = 'Anomaly\ExampleModule\Example\ExampleModel';
 
+    protected $fields = [
+        '*',
+        'test' => [
+            'label'        => 'Test Input',
+            'instructions' => 'This is a test input you can mess with a bit.',
+            'type'         => 'markdown',
+            'config'       => [
+                'separator' => '5'
+            ]
+        ]
+    ];
+
     protected $actions = ['save'];
 }
